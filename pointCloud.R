88bf7432874de71d8da133a0c55c2880071cb6d7
sk956 <sk956@cornell.edu>
2021-03-13 22:27:09 -0500

    Draw line graph in R

--- pointCloud.R
+++ pointCloud.R
@@ -3,3 +3,13 @@
 library(ggplot2) 
 
 # brin the csv file 3 different csv files
+tableP1 = read.csv("/Users/rentaluser/Desktop/masterThesis/emotionCSV/processed/testing/p1.csv")
+tableP2 = read.csv("/Users/rentaluser/Desktop/masterThesis/emotionCSV/processed/testing/p2.csv")
+tableP3  = read.csv("/Users/rentaluser/Desktop/masterThesis/emotionCSV/processed/testing/p3.csv")
+
+ggplot() + 
+  geom_line(data = tableP1, aes(x = min_timestamp, y = emotions ), color = 'red') + 
+  geom_line(data = tableP2, aes(x = min_timestamp, y = emotions ), color = 'blue') +
+  geom_line(data = tableP3, aes(x = min_timestamp, y = emotions ), color = 'black') + 
+  xlab('minute') + 
+  ylab('emotion')

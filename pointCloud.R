f4b79abf5cf981bef9257b66fa7d6fd81639bc0a
sk956 <sk956@cornell.edu>
2021-03-14 23:59:35 -0400

    processsed_CSV create + create line graph

--- pointCloud.R
+++ pointCloud.R
@@ -3,15 +3,16 @@
 library(ggplot2) 
 
 # brin the csv file 3 different csv files
-tableP1 = read.csv("/Users/rentaluser/Desktop/masterThesis/emotionCSV/processed/testing/p1.csv")
-tableP2 = read.csv("/Users/rentaluser/Desktop/masterThesis/emotionCSV/processed/testing/p2.csv")
-tableP3  = read.csv("/Users/rentaluser/Desktop/masterThesis/emotionCSV/processed/testing/p3.csv")
+tableP1 = read.csv("/Users/rentaluser/desktop/masterThesis/emotionCSV/processed/pilot_study/pilotteam1r2_p1.csv")
+tableP2 = read.csv("/Users/rentaluser/desktop/masterThesis/emotionCSV/processed/pilot_study/pilotteam1r2_p2.csv")
+tableP3  = read.csv("/Users/rentaluser/desktop/masterThesis/emotionCSV/processed/pilot_study/pilotteam1r2_p3.csv")
 
 
 # look at the videos that go bad 
 ggplot() + 
-  geom_line(data = tableP1, aes(x = min_timestamp, y = emotions ), color = 'red') + 
-  geom_line(data = tableP2, aes(x = min_timestamp, y = emotions ), color = 'blue') +
-  geom_line(data = tableP3, aes(x = min_timestamp, y = emotions ), color = 'black') + 
+  geom_line(data = tableP1, aes(x = min_upgrade, y = emotions ), color = 'blue') + 
+  geom_line(data = tableP2, aes(x = min_upgrade, y = emotions ), color = 'red') +
+  geom_line(data = tableP3, aes(x = min_upgrade, y = emotions ), color = 'black') + 
   xlab('minute') + 
   ylab('emotion')
+

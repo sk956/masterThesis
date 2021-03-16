6a27053eb315139d88dd8894c205d7a70414ff4b
sk956 <sk956@cornell.edu>
2021-03-16 00:14:53 -0400

    WIP

---
+++ linearRegression.R
@@ -0,0 +1,10 @@
+# create a linear regression for the three person 
+# to create a linear regression model for the three people -> 
+# created a new csv file where each column is p1,p2 and p3 for the cumulative 
+# emotions
+library(ggplot2) 
+
+tableCum = read.csv("/Users/rentaluser/Desktop/masterThesis/emotionCSV/processed/pilot_study/pilotTeam1r2/pilotteam1r2.csv")
+
+# linear regression for all three columns 
+
--- pointCloud.R
+++ pointCloud.R
@@ -3,9 +3,9 @@
 library(ggplot2) 
 
 # brin the csv file 3 different csv files
-tableP1 = read.csv("/Users/rentaluser/desktop/masterThesis/emotionCSV/processed/pilot_study/pilotteam1r2_Full_p1.csv")
-tableP2 = read.csv("/Users/rentaluser/desktop/masterThesis/emotionCSV/processed/pilot_study/pilotteam1r2_Full_p2.csv")
-tableP3  = read.csv("/Users/rentaluser/desktop/masterThesis/emotionCSV/processed/pilot_study/pilotteam1r2_Full_p3.csv")
+tableP1 = read.csv("/Users/rentaluser/desktop/masterThesis/emotionCSV/processed/pilot_study/pilotTeam1r2/pilotteam1r2_Full_p1.csv")
+tableP2 = read.csv("/Users/rentaluser/desktop/masterThesis/emotionCSV/processed/pilot_study/pilotTeam1r2/pilotteam1r2_Full_p2.csv")
+tableP3  = read.csv("/Users/rentaluser/desktop/masterThesis/emotionCSV/processed/pilot_study/pilotTeam1r2/pilotteam1r2_Full_p3.csv")
 
 
 # look at the videos that go bad 
@@ -25,16 +25,27 @@ model <- lm(cum_emotions ~min_upgrade, data = tableP1)
 model2 <- lm(cum_emotions ~min_upgrade, data = tableP2)
 model3 <-  lm(cum_emotions ~min_upgrade, data = tableP3)
 
-ggp <- ggplot(tableP1, aes(x = min_upgrade, y = cum_emotions, color = 'red'  )) + 
+# 3 different graphs linear regression lines
+ggp <- ggplot(tableP1, aes(x = min_upgrade, y = cum_emotions  )) + 
   geom_point() 
-ggp + stat_smooth(method  = "lm", formula = y~x)
-
-gg2 <- ggplot(tableP2, aes(x = min_upgrade, y = cum_emotions, color = 'blue'  )) + 
-  geom_point() 
-gg2 + stat_smooth(method  = "lm", formula = y~x)
-
-
+ggp <-ggp + stat_smooth(method  = "lm", formula = y~x)
 
 summary(model)
+ggp + annotate(geom="text", x=20, y= 1000, label="P1, 198.4354 ",
+             color="red")
+
+
+gg2 <- ggplot(tableP2, aes(x = min_upgrade, y = cum_emotions )) + 
+  geom_point() 
+gg2 <- gg2 + stat_smooth(method  = "lm", formula = y~x)
 summary(model2)
+gg2 + annotate(geom="text", x=20, y= 1000, label="P2,  232.697 ",
+               color="red")
+
+gg3 <- ggplot(tableP3, aes(x = min_upgrade, y = cum_emotions )) + 
+  geom_point() 
+gg3 <- gg3+ stat_smooth(method  = "lm", formula = y~x)
 summary(model3)
+gg3 + annotate(geom="text", x=20, y= 1000, label="P3, 152.590  ",
+               color="red")
+
